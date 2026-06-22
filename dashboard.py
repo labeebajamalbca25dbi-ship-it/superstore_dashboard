b948cef8890539b1ec1acbef467c7f32cc52fa9a
labeeba <labeebajamal.bca25.dbi@gmail.com>
2026-06-22 15:21:15 +0530

    message

--- dashboard.py
+++ dashboard.py
@@ -9,7 +9,7 @@ st.set_page_config(page_title="Superstore Dashboard",page_icon="📊",layout="wi
 
 @st.cache_data
 def load_data():
-    df=pd.read_csv("data\superstore_clean.csv",parse_dates=["Order Date","Ship Date"])
+    df=pd.read_csv("superstore_clean.csv",parse_dates=["Order Date","Ship Date"])
     return df
 
 
